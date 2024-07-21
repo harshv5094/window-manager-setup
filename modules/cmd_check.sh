4611432bef1ec3ff9f1622e122b076d0e25fb846
harshv5094 (Fedora Personal PC) <harshv5094@gmail.com>
2024-07-21 19:38:15 +0530

    fix(cmd_check.sh): fixing sourcing colors.sh problem
    I was fetching the colors.sh from another git repo since I've deleted from that repo now that I'm
    using ansible, I'm now properly fetching the original file from the modules directory in the wm

--- modules/cmd_check.sh
+++ modules/cmd_check.sh
@@ -1,48 +1,48 @@
 #!/usr/bin/env bash
 
-source ~/.dotfiles/.scripts/modules/colors.sh
+source ~/GitHub/wm/.scripts/modules/colors.sh
 
 function delete_folder_check() {
-	text=$1
-	if [ $? -eq 0 ]; then
-		echo_success "Successfully Deleted $text Config Folder"
-	else
-		echo_error "Failed to Delete $text Config Folder"
-	fi
+  text=$1
+  if [ $? -eq 0 ]; then
+    echo_success "Successfully Deleted $text Config Folder"
+  else
+    echo_error "Failed to Delete $text Config Folder"
+  fi
 }
 
 function link_folder_check() {
-	text=$1
-	if [ $? -eq 0 ]; then
-		echo_success "Successfully Linked $text Config Folder"
-	else
-		echo_error "Failed to Link $text Config Folder"
-	fi
+  text=$1
+  if [ $? -eq 0 ]; then
+    echo_success "Successfully Linked $text Config Folder"
+  else
+    echo_error "Failed to Link $text Config Folder"
+  fi
 }
 
 function link_file_check() {
-	text=$1
-	if [ $? -eq 0 ]; then
-		echo_success "Successfully Linked $text"
-	else
-		echo_error "Failed to Link $text"
-	fi
+  text=$1
+  if [ $? -eq 0 ]; then
+    echo_success "Successfully Linked $text"
+  else
+    echo_error "Failed to Link $text"
+  fi
 }
 
 function delete_file_check() {
-	text=$1
-	if [ $? -eq 0 ]; then
-		echo_success "Deleted $text"
-	else
-		echo_error "Failed to Delete $text"
-	fi
+  text=$1
+  if [ $? -eq 0 ]; then
+    echo_success "Deleted $text"
+  else
+    echo_error "Failed to Delete $text"
+  fi
 }
 
 function copy_file_check() {
-	text=$1
-	if [ $? -eq 0 ]; then
-		echo_success "Copied $text"
-	else
-		echo_error "Failed to Copy $text"
-	fi
+  text=$1
+  if [ $? -eq 0 ]; then
+    echo_success "Copied $text"
+  else
+    echo_error "Failed to Copy $text"
+  fi
 }
